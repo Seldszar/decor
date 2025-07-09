83d625525ed3b947a8f885e054e3ec3cc29ea16e
Seldszar <contact@seldszar.fr>
2025-07-09 14:39:51 +0200

    fix: page

--- rsbuild.config.ts
+++ rsbuild.config.ts
@@ -3,4 +3,18 @@ import { pluginPreact } from '@rsbuild/plugin-preact';
 
 export default defineConfig({
   plugins: [pluginPreact()],
+
+  html: {
+    title: "Decor: Theme Editor",
+
+    tags: [
+      {
+        tag: "meta",
+        attrs: {
+          name: "description",
+          content: "Yet another Discord Streamkit Overlay theme editor",
+        },
+      },
+    ],
+  },
 });
